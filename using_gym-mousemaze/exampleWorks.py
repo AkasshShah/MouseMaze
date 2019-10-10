bac15454673f0da177623b256eb5641bf64024d8
BlackBird2211 <akassh.shah77@gmail.com>
2019-10-09 23:44:27 -0400

    showing better example

--- using_gym-mousemaze/exampleWorks.py
+++ using_gym-mousemaze/exampleWorks.py
@@ -1,4 +1,5 @@
 import gym
 if __name__ == '__main__':
     env = gym.make('gym_mousemaze:mm-v1')
-    env.render()
+    if(env.encode(3, (0, 0), [(1, 1), (2, 2)], [(0, 1), (1, 0)], [((0, 0), (0, 1))])):
+        env.render()
